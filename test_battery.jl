3ddb4f6a94d8085e0c61aa389ead643dea1a7f1e
solenedp <71374746+solenedp@users.noreply.github.com>
2023-04-20 16:28:06 +0200

    very simple example

---
+++ test_battery.jl
@@ -0,0 +1,58 @@
+using SDDP
+
+Q = 5 # initial amount of money invested 
+C_max = 5 # capacity of the storage
+r = 0.05
+l = 0.05
+T = 5
+
+function subproblem_builder(subproblem::Model, node::Int)
+    # State variables
+    @variable(subproblem, Z, SDDP.State, initial_value = -Q)
+    @variable(subproblem, 0 <= C <= C_max, SDDP.State, initial_value = 0)
+    # Control variables
+    @variable(subproblem, -C_max <= U <= C_max) # units of electricity brought from the electricity market 
+    # Random variables
+    @variable(subproblem,unit_price)
+    # the prices are low every other day. 
+    Ω1 = [1.0, 1.5, 2.0]
+    Ω2 = [2.5, 3.0, 3.5]
+    P = [1 / 3, 1 / 3, 1 / 3]
+    if node%2 == 0
+        SDDP.parameterize(subproblem, Ω1, P) do ω
+            return JuMP.fix(unit_price, ω)
+        end
+    else
+        SDDP.parameterize(subproblem, Ω2, P) do ω
+            return JuMP.fix(unit_price, ω)
+        end
+    end
+
+    # Transition function and constraints
+    @constraints(
+        subproblem,
+        begin
+            Z.out == (1+r)*Z.in - unit_price*U # balance equation for the invested money
+            C.out == (1 - l)*C.in + U # balance equation for the storage
+        end
+    )
+    # Stage-objective
+    if node == T
+        @stageobjective(subproblem, Z.out) # maximize the final amount of money of the investor 
+    else
+        @stageobjective(subproblem,0.0)
+    end
+    return subproblem
+end
+
+using Gurobi
+
+model = SDDP.LinearPolicyGraph(
+    subproblem_builder;
+    stages = T,
+    sense = :Max,
+    upper_bound = 1000.0,
+    optimizer = Gurobi.Optimizer,
+)
+
+SDDP.train(model; iteration_limit = 10)
